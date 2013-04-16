3f6c3961dac02768c15e7e27c11568dc3e8f39a8
narkisr <narkisr@gmail.com>
2013-04-16 23:41:01 +0300

    adding demo example

---
+++ fixtures/supernal-demo.clj
@@ -0,0 +1,15 @@
+(env 
+  {:roles {
+      :web #{
+             {:host "192.168.1.26" :user "vagrant" :sudo true}
+             {:host "192.168.1.27" :user "vagrant" :sudo true}}}
+   })
+
+(ns- deploy 
+  (task stop
+    (debug "stopping service" remote)
+    (run "hostname")))
+
+(def artifact "git://github.com/narkisr/swag.git")
+
+(execute basic-deploy {:app-name "foo" :src artifact} :web :join true)
