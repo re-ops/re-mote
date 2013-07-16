1235b58b5b2cf80fc86e7fa5abb58559648614c4
Ronen Narkis <narkisr@gmail.com>
2013-07-16 05:57:08 +0300

    version bump

--- src/supernal/launch.clj
+++ src/supernal/launch.clj
@@ -73,7 +73,7 @@
 (defcommand version 
   "List supernal version and info" 
   {:opts-spec [] :bind-args-to [script]}
-  (println "Supernal 0.2.6"))
+  (println "Supernal 0.2.7"))
 
 (defn -main [& args]
   (binding [*ns* (create-ns 'supernal.adhoc)] 
