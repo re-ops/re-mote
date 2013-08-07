b85a744917c7b30b57c643103d6f404ebf33499a
Ronen Narkis <narkisr@gmail.com>
2013-08-07 19:05:59 +0300

    version up

--- src/supernal/launch.clj
+++ src/supernal/launch.clj
@@ -94,7 +94,7 @@
                   sup version 
   " 
   {:opts-spec [] :bind-args-to [script]}
-  (println "Supernal 0.2.8"))
+  (println "Supernal 0.2.9"))
 
 (defn -main [& args]
   (binding [*ns* (create-ns 'supernal.adhoc)] 
