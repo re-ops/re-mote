0d13ca82997b55df72ff9e5a243d0c4c29673ad2
narkisr <narkisr@gmail.com>
2013-12-26 12:35:57 +0200

    ok with remote data

--- test/supernal/integration/tasks.clj
+++ test/supernal/integration/tasks.clj
@@ -28,15 +28,16 @@
 (defn extract-error [{:keys [fail]}]
   {:fail (-> fail bean :message)})
 
+(def ok [{:ok nil, :remote  {:host "192.168.2.26", :sudo true, :user "vagrant"}}])
 (fact "base deployment tasks no join" :integration :supernal
-   (execute base-deploy {:app-name "foo" :src artifact} :web) => [{:ok nil}])
+   (execute base-deploy {:app-name "foo" :src artifact} :web) => ok)
 
 (fact "single task" :integration :supernal
-   (execute-task deploy/stop {:app-name "foo" :src artifact} :web) => [{:ok nil}])
+   (execute-task deploy/stop {:app-name "foo" :src artifact} :web) => ok)
 
 (fact "env option" :integration :supernal
    (let [e {:roles {:web #{{:host "192.168.2.26" :user "vagrant" :sudo true}}} }]
-     (execute-task deploy/stop {:app-name "foo" :src artifact} :web :env e) => [{:ok nil}]))
+     (execute-task deploy/stop {:app-name "foo" :src artifact} :web :env e) => ok))
 
 (fact "task with error" :integration :supernal
   (map extract-error (execute-task error/zero-div {:app-name "foo" :src artifact} :web)) => [{:fail "Divide by zero"}])
