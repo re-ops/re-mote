87011beb4002862602afe7b2618806ac1815619e
ronen <narkisr@gmail.com>
2015-05-31 19:17:56 +0300

    updating midje, clojure and core.async to lastest versions

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A remote multi server automation tool (like Capistrano/Fabric)"
   :url "https://github.com/celestial-ops/supernal"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.bouncycastle/bcprov-jdk16 "1.46"]
                  [org.clojure/core.incubator "0.1.2"]
                  [com.taoensso/timbre "1.5.2"]
@@ -12,17 +12,17 @@
                  [com.hierynomus/sshj "0.12.0"] 
                  [clj-aws-s3 "0.3.6"]
                  [me.raynes/conch "0.5.0"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [narkisr/cliopatra "1.1.0"]
                  [narkisr/clansi "1.2.0"] ]
 
   :exclusions [org.clojure/clojure]
 
-  :plugins  [[jonase/eastwood "0.0.2"] [lein-midje "3.0.0"] [lein-tag "0.1.0"]
+  :plugins  [[jonase/eastwood "0.0.2"] [lein-midje "3.1.3"] [lein-tag "0.1.0"]
              [lein-tar "2.0.0"] [lein-set-version "0.3.0"] ]
 
   :profiles {:dev { 
-              :dependencies [[midje "1.5.1"] [junit/junit "4.8.1"] ]
+              :dependencies [[midje "1.6.3"] [junit/junit "4.8.1"] ]
               :jvm-opts ~(vec (map (fn [[p v]] (str "-D" (name p) "=" v)) {:disable-conf "true"}))
               :resource-paths  ["pkg/etc/"]
               :source-paths  ["dev"]           
@@ -36,13 +36,11 @@
     :init-ns user               
   }
   
-  :aliases {"autotest"
-            ["midje" ":autotest" ":filter" "-integration"] 
-            "runtest"
-            ["midje" ":filter" "-integration"] 
-            "supernal"
-            ["run" "-m" "supernal.launch"] 
-            }
+  :aliases {
+     "autotest" ["midje" ":autotest" ":filter" "-integration"] 
+     "runtest" ["midje" ":filter" "-integration"] 
+     "supernal" ["run" "-m" "supernal.launch"] 
+   }
 
   :aot [supernal.launch]
 
