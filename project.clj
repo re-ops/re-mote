2f8a5bf352cb4836b40d18b1d3b20577a79a0f4e
ronen <narkisr@gmail.com>
2015-06-21 18:40:04 +0300

    latest non breaking deps

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "https://github.com/celestial-ops/supernal"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.incubator "0.1.2"]
-                 [com.taoensso/timbre "1.5.2"]
-                 [ch.qos.logback/logback-classic "1.0.9"]
-                 [org.codehaus.groovy/groovy "2.1.2"]
+                 [org.clojure/core.incubator "0.1.3"]
+                 [com.taoensso/timbre "2.6.3"]
+                 [ch.qos.logback/logback-classic "1.1.3"]
+                 [org.codehaus.groovy/groovy "2.4.3"]
                  [prismatic/plumbing "0.0.1"]
                  [com.hierynomus/sshj "0.12.0"] 
-                 [clj-aws-s3 "0.3.6"]
                  [me.raynes/conch "0.5.0"]
+                 [clj-aws-s3 "0.3.6"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [narkisr/cliopatra "1.1.0"]
                  [narkisr/clansi "1.2.0"] ]
@@ -18,10 +18,11 @@
   :exclusions [org.clojure/clojure]
 
   :plugins  [[jonase/eastwood "0.0.2"] [lein-midje "3.1.3"] [lein-tag "0.1.0"]
+             [lein-ancient "0.6.7" :exclusions [org.clojure/clojure]] 
              [lein-tar "2.0.0"] [lein-set-version "0.3.0"] ]
 
   :profiles {:dev { 
-              :dependencies [[midje "1.6.3"] [junit/junit "4.8.1"] ]
+              :dependencies [[midje "1.6.3"] [junit/junit "4.12"] ]
               :jvm-opts ~(vec (map (fn [[p v]] (str "-D" (name p) "=" v)) {:disable-conf "true"}))
               :resource-paths  ["pkg/etc/"]
               :source-paths  ["dev"]           
