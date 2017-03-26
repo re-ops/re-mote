7a29968df5cad0ada3d046027058a092ff865ccf
ronen <narkisr@gmail.com>
2017-03-27 01:45:18 +1100

    killing comments on cljs files

--- src/re_mote/publish/client.cljs
+++ src/re_mote/publish/client.cljs
@@ -1,16 +1,5 @@
-(comment
-   re-mote, Copyright 2017 Ronen Narkis, narkisr.com
-   Licensed under the Apache License,
-   Version 2.0  (the "License") you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.)
-
 (ns supernal.publish.client
-  "client publish code" 
+  "client publish code"
   (:require
    [clojure.string  :as str]
    [cljs.core.async :as async  :refer (<! >! put! chan)]
@@ -26,11 +15,11 @@
 
 (timbre/debug "ClojureScript appears to have loaded correctly.")
 
-(let [{:keys [chsk ch-recv send-fn state]} 
+(let [{:keys [chsk ch-recv send-fn state]}
       (sente/make-channel-socket-client!  "/chsk" {:type :ws :packer :edn})]
   (def chsk chsk)
-  (def ch-chsk ch-recv) 
-  (def chsk-send! send-fn) 
+  (def ch-chsk ch-recv)
+  (def chsk-send! send-fn)
   (def chsk-state state))
 
 (defmulti msg-handler :id)
