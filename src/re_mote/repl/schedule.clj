1a02483263c5ff6beae622f6bfb3a3d744140e2b
ronen <narkisr@gmail.com>
2017-05-09 21:24:37 +1000

    pprint results

--- src/re_mote/repl/schedule.clj
+++ src/re_mote/repl/schedule.clj
@@ -71,7 +71,9 @@
 
 (defn schedule-report []  
   (doseq [[k {:keys [result period]}] @status]
-    (when result (println "Last run result of" (name k) result)))
+    (when result 
+      (println "Result of" (name k) ":") 
+      (pprint result)))
   (println " ")
   (doseq [[k {:keys [result period]}] (sort-by (fn [[k m]] (first (m :period))) @status)]
     (let [date (f/unparse (f/formatter-local "dd/MM/YY HH:mm:ss") (first period))]
