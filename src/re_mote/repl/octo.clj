088eef0905a39e2f105e658d8106666a56162969
ronen <narkisr@gmail.com>
2017-05-23 00:42:55 +1000

    octo file parameter

--- src/re_mote/repl/octo.clj
+++ src/re_mote/repl/octo.clj
@@ -1,21 +1,22 @@
 (ns re-mote.repl.octo
   "Basic octo (git backup tool)"
   (:require 
+    [clojure.core.strint :refer (<<)]
     [re-mote.repl.base :refer (refer-base)]
     [re-mote.repl.publish :refer (email)]
     [pallet.stevedore :refer (script)]))
 
 (refer-base)
 
-(defn octo-script []
+(defn octo-script [f]
   (script
     (chain-and 
-      ("/usr/bin/octo" "sync" "/etc/octo.edn") 
-      ("/usr/bin/octo" "push" "/etc/octo.edn")
-      ("/usr/bin/octo" "pull" "/etc/octo.edn"))))
+      ("/usr/bin/octo" "sync" ~f) 
+      ("/usr/bin/octo" "push" ~f)
+      ("/usr/bin/octo" "pull" ~f))))
 
-(defn full-cycle [host tofrom]
-  (run (exec host (octo-script)) | (email (tofrom "octo sync"))))
+(defn full-cycle [host f tofrom]
+  (run (exec host (octo-script f)) | (email (tofrom (<< "octo ~{f} sync")))))
 
 (defn refer-octo []
   (require '[re-mote.repl.octo :as octo :refer (full-cycle)]))
