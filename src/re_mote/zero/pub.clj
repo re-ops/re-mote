04acdcfcbb2f9b6a9d79c1a2b4d6737ea9c0b523
ronen <narkisr@gmail.com>
2017-06-05 01:50:31 +1000

    client server

--- src/re_mote/zero/pub.clj
+++ src/re_mote/zero/pub.clj
@@ -1,31 +1,44 @@
 (ns re-mote.zero.pub
   "publish to agents"
   (:require
-   [re-mote.zero.common :refer (read-key context)])
-  (:import 
+    [taoensso.timbre :refer (refer-timbre)]
+   [re-mote.zero.common :refer (read-key context close!)])
+  (:import
     [org.zeromq ZMQ]
-    [java.nio.charset Charset])
-  )
+    [java.nio.charset Charset]))
 
+(refer-timbre)
+
+(defn server-socket [t private]
+   [t private]
+  (doto
+    (.socket (context) t)
+    (.setCurveServer true)
+    (.setCurveSecretKey (read-key private))))
 
 (defn pub-socket [private]
-  (doto
-    (.socket (context) ZMQ/PUB)
-    (.setCurveServer true)
-    (.setCurveSecretKey (read-key private))
+  (doto (server-socket ZMQ/PUB private)
     (.setZAPDomain (.getBytes "global")) ;
-    (.bind "tcp://*:9000")
-    ))
+    (.bind "tcp://*:9000")))
 
-(declare socket)
+(defn rep-socket [private]
+  (doto (server-socket ZMQ/REP private)
+    (.bind "tcp://*:9001")))
 
-(defn setup [private]
-  (def socket (atom (pub-socket private))))
+(def sockets (atom {}))
+
+(defn setup-server [private]
+  (reset! sockets
+    {:rep (rep-socket private) :pub (pub-socket private)}))
 
 (defn publish [msg topic]
-   (.sendMore @socket topic)
-   (.send @socket msg))
+  (let [{:keys [pub rep]} @sockets]
+    (.sendMore pub topic)
+    (.send pub msg)
+    (info (.recvStr rep 0 (Charset/defaultCharset)))
+    ))
 
-(comment 
-  (setup ".curve/server-private.key")
-  (publish "/bin/ls /" "play"))
+(comment
+  (setup-server ".curve/server-private.key")
+  (close! @sockets)
+  (future (publish "/bin/ls /" "play")))
