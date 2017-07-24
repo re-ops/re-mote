72f27b8f5af65220ac646dca982eca4c633f56eb
ronen <narkisr@gmail.com>
2017-07-25 00:39:46 +1000

    indent fix

--- src/re_mote/repl/re_gent.clj
+++ src/re_mote/repl/re_gent.clj
@@ -8,7 +8,7 @@
   (:import [re_mote.repl.base Hosts]))
 
 (defprotocol Regent
-  (stop 
+  (stop
     [this]
     [this m])
   (re-start
@@ -17,28 +17,28 @@
 
 (defn kill-script []
   (script
-    (chain-or
-      (pipe (pipe ("ps" "aux") ("awk" "'/re-gent/ {print $2}'")) ("xargs" "kill" "-9")) true)))
+   (chain-or
+    (pipe (pipe ("ps" "aux") ("awk" "'/re-gent/ {print $2}'")) ("xargs" "kill" "-9")) true)))
 
 (defn start-script [port home]
   (let [bin (<< "~{home}/re-gent") cmd (<< "\"~{bin} ${IP} ~{port} &\"")]
     (script
-      (set! IP @(pipe ("echo" "$SSH_CLIENT") ("awk" "'{print $1}'")))
-      ("chmod" "+x"  ~bin)
-      ("nohup" "sh" "-c" ~cmd "&>/dev/null")
-      (if (not (= $? 0)) ("exit" 1)))))
+     (set! IP @(pipe ("echo" "$SSH_CLIENT") ("awk" "'{print $1}'")))
+     ("chmod" "+x"  ~bin)
+     ("nohup" "sh" "-c" ~cmd "&>/dev/null")
+     (if (not (= $? 0)) ("exit" 1)))))
 
 (defn re-start-script [port home]
   (chained-script
-    (kill-script) 
-    (start-script port home)))
+   (kill-script)
+   (start-script port home)))
 
 (extend-type Hosts
   Regent
   (re-start [this]
     (launch this {}))
   (re-start [this _ home]
-   [this (run-hosts this (re-start-script @front-port home))]))
+    [this (run-hosts this (re-start-script @front-port home))]))
 
 (defn refer-regent []
   (require '[re-mote.repl.re-gent :as re-gent :refer (re-start stop)]))
