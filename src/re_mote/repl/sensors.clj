3ec47bdd4774ec3dea41b433dba48e2301787adc
ronen <narkisr@gmail.com>
2017-08-06 20:09:02 +1000

    indent fix

--- src/re_mote/repl/sensors.clj
+++ src/re_mote/repl/sensors.clj
@@ -11,10 +11,10 @@
   (script
    ("cat" "/proc/cpuinfo" | "grep" "hypervisor")
    (if (= "$?" 0)
-   (do
-     (println "'cannot measure temp in a VM'")
-     ("exit" 1))
-    (pipe ("sensors -A") ("awk" "'{$1=$1};1'")))))
+     (do
+       (println "'cannot measure temp in a VM'")
+       ("exit" 1))
+     (pipe ("sensors -A") ("awk" "'{$1=$1};1'")))))
 
 (defn parse-lines [lines]
   (mapv
