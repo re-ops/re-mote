59db0bb4b23184a51f54c3ac3f571fb55510695c
ronen <narkisr@gmail.com>
2017-10-07 20:28:03 +1100

    dynamic bucket creation and build

--- src/re_mote/zero/results.clj
+++ src/re_mote/zero/results.clj
@@ -7,20 +7,30 @@
 
 (refer-timbre)
 
-(def results (atom {}))
 
-(defn add-result [hostname name id r t]
-  (let [v {:r r :t t}]
-    (if-let [a (get-in @results [(keyword name) id])]
+(def buckets 32)
+
+(def results 
+  (into {} (map (fn [i] [i (atom {})]) (range buckets))))
+
+(defn capacity []
+  (map (fn [[k v]] (count @v)) results))
+
+(defn bucket [uuid]
+  (results (mod (BigInteger. uuid 16) buckets)))
+
+(defn add-result [hostname name uuid r t]
+  (let [v {:r r :t t} b (bucket uuid)]
+    (if-let [a (get-in @b [(keyword name) uuid])]
       (swap! a assoc hostname v)
-      (swap! results assoc-in [(keyword name) id] (atom {hostname v})))))
+      (swap! b assoc-in [(keyword name) uuid] (atom {hostname v})))))
 
-(defn- result [uuid k]
-  (if-let [a (get-in @results [k uuid])] @a {}))
+(defn result [uuid k]
+  (if-let [a (get-in @(bucket uuid) [k uuid])] @a {}))
 
 (defn clear-results
-  [uuid k hs]
-  (swap! results dissoc-in [k uuid]))
+  [hs k uuid]
+  (swap! (bucket uuid) dissoc-in [k uuid]))
 
 (defn get-results [{:keys [hosts]} k uuid]
   (let [ks (set (keys (result uuid k)))]
@@ -38,4 +48,4 @@
    (let [r (result k uuid)] (r host))))
 
 (defn refer-zero-results []
-  (require '[re-mote.zero.results :as zerors :refer (pretty-result clear-results add-result get-results missing-results)]))
+  (require '[re-mote.zero.results :as zerors :refer (pretty-result clear-results add-result get-results missing-results capacity)]))
