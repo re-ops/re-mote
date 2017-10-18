58e3261ef04d9d51a8a8baea1610b039ed60ab47
ronen <narkisr@gmail.com>
2017-10-19 02:09:49 +1100

    using formation

--- src/re_mote/persist/es.clj
+++ src/re_mote/persist/es.clj
@@ -1,6 +1,7 @@
 (ns re-mote.persist.es
   "Persisting results into Elasticsearch"
   (:require
+    [formation.core :as form]
     [re-mote.log :refer (gen-uuid)]
     [qbits.spandex :as s]
     [taoensso.timbre :refer (refer-timbre)])
@@ -8,27 +9,39 @@
 
 (refer-timbre)
 
-
 (defprotocol Elasticsearch
   (persist
     [this m])
   (query
-    [this q])
-  )
+    [this q]))
 
+(def c (atom nil))
 
-(def server "http://127.0.0.1:9208" )
-(def index "re-mote")
-(def type- "results")
-
-(def c (s/client  {:hosts [server]}))
+(def es
+  (memoize (fn [k] ((:es (form/config "re-mote" (fn [_] nil))) k))))
 
 (extend-type Hosts
   Elasticsearch
    (persist [this m]
-     (s/request c {:url [index type- (gen-uuid)] :method :post :body m})
-     [this m]
-     ))
+     (let [req {:url [(es :index) (es :type) (gen-uuid)] :method :post :body m}
+           {:keys [status] :as res} (s/request @c req)]
+       (if (#{200 201} status)
+        [this m]
+        (throw (ex-info "failed to persist results" res) )
+         ))))
 
+(defn start []
+  (reset! c (s/client {:hosts [(es :server)] :basic-auth {:user "elastic" :password "changeme"}})))
+
+(defn stop []
+  (reset! c nil))
+ 
 (defn refer-es-persist []
   (require '[re-mote.persist.es :as es :refer (persist)]))
+
+
+(comment
+  (start) 
+  (s/request @c 
+    {:url [(es :index) (es :type) (gen-uuid)] 
+     :method :post :body {:results "hello!"}}))
