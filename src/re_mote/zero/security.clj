feb88a4ff20aadf8aef6129af5726833800a83b1
ronen <narkisr@gmail.com>
2018-01-15 02:28:52 +1100

    ports

--- src/re_mote/zero/security.clj
+++ src/re_mote/zero/security.clj
@@ -5,9 +5,24 @@
    [re-mote.zero.pipeline :refer (run-hosts)]
    [taoensso.timbre :refer (refer-timbre)]
    [re-mote.zero.shell :refer (args)]
+   [re-mote.zero.stats :refer (zip)]
    [re-mote.scripts.security :refer (ufw-script)]
    [re-mote.zero.functions :refer (shell)])
   (:import [re_mote.repl.base Hosts]))
+
+(refer-timbre)
+
 (defprotocol Security
-  (firewall [this] [this m])
+  (ports [this] [this m])
   (ssh-logins [this] [this m]))
+
+(def timeout [5 :second])
+
+(extend-type Hosts
+  Security
+  (ports
+    ([this]
+     (zip this
+          (run-hosts this shell (args ufw-script) timeout)
+          :security :ports :from :action :to))))
+
