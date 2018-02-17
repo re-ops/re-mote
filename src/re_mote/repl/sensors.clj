2565f1f197e8797f7a217d81ef0b43d15ad36e6b
ronen <narkisr@gmail.com>
2018-02-18 00:14:50 +1100

    ssh based temp sensing

---
+++ src/re_mote/repl/sensors.clj
@@ -0,0 +1,20 @@
+(ns re-mote.repl.sensors
+  "Sensors monitoring over ssh"
+  (:require
+   [re-mote.ssh.pipeline :refer (run-hosts)]
+   [re-mote.scripts.sensors :refer (temp-script)]
+   [re-mote.zero.sensors :refer (assoc-stats into-dec)])
+  (:import
+   [re_mote.repl.base Hosts]))
+
+(defprotocol Sensors
+  (temperature [this]))
+
+(extend-type Hosts
+  Sensors
+  (temperature [this]
+    (let [{:keys [success failure] :as res} (run-hosts this (temp-script))]
+      (into-dec [this (assoc res :success (map assoc-stats success))]))))
+
+(defn refer-sensors []
+  (require '[re-mote.repl.sensors :as sens]))
