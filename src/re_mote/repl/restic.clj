2ba1cd1bf694cf2b1669f0c2d3f6e1eb23f51f45
ronen <narkisr@gmail.com>
2018-03-23 22:21:00 +1100

    Adding check and dynamic export generation by type

--- src/re_mote/repl/restic.clj
+++ src/re_mote/repl/restic.clj
@@ -1,6 +1,7 @@
 (ns re-mote.repl.restic
   "Restic https://restic.net/ operations"
   (:require
+   [clojure.string :refer (upper-case)]
    [re-mote.ssh.pipeline :refer (run-hosts)]
    [clojure.core.strint :refer (<<)]
    [re-mote.repl.base :refer (refer-base)]
@@ -11,28 +12,49 @@
 (defn escape [s]
   (str \" s \"))
 
-(defn restic-script
+(defn prefix [t suffix]
+  (<< "~(upper-case (name t))_~{suffix}"))
+
+(defn backup-script
   "restic backup script"
   [{:keys [src dest pass id key type] :as b}]
   (let [target (<< "~{type}:~{dest}")]
     (script
      ("export" (set! RESTIC_PASSWORD ~(escape pass)))
-     ("export" (set! B2_ACCOUNT_KEY ~(escape key)))
-     ("export" (set! B2_ACCOUNT_ID ~(escape id)))
+     ("export" (set! ~(prefix type "ACCOUNT_KEY") ~(escape key)))
+     ("export" (set! ~(prefix type "ACCOUNT_ID") ~(escape id)))
      ("/usr/bin/restic" "backup" ~src "-r" ~target))))
+
+(defn check-script
+  "restic backup script"
+  [{:keys [dest pass id key type] :as b}]
+  (let [target (<< "~{type}:~{dest}")]
+    (script
+     ("export" (set! RESTIC_PASSWORD ~(escape pass)))
+     ("export" (set! ~(prefix type "ACCOUNT_KEY") ~(escape key)))
+     ("export" (set! ~(prefix type "ACCOUNT_ID") ~(escape id)))
+     ("/usr/bin/restic" "check" "-r" ~target))))
 
 (defprotocol Restic
   (backup
+    [this b]
+    [this m b])
+  (check
     [this b]
     [this m b]))
 
 (extend-type Hosts
   Restic
+  (check
+    ([this b]
+     (backup this {} b))
+    ([this m b]
+     [this (run-hosts this (check-script b))]))
   (backup
     ([this b]
      (backup this {} b))
     ([this m b]
-     [this (run-hosts this (restic-script b))])))
+     [this (run-hosts this (backup-script b))])))
 
 (defn refer-restic []
-  (require '[re-mote.repl.restic :as rst :refer (backup)]))
+  (require '[re-mote.repl.restic :as rst :refer (backup check)]))
