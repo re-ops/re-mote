a087d16bee434edc17cc9db99343c67c5ec9a53b
ronen <narkisr.com>
2018-10-26 21:57:04 +1100

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-mote "0.8.9"
+(defproject re-mote "0.9.0"
   :description "A live remote operations environment"
   :url "https://github.com/re-ops/re-mote"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
