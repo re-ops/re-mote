2966339ffcf9f8f3ecffcb7146b7e2f48eb66c15
ronen <narkisr@gmail.com>
2019-01-19 00:12:58 +1100

    initial stats events support

--- src/re_mote/publish/riemann.clj
+++ src/re_mote/publish/riemann.clj
@@ -5,11 +5,26 @@
    [clj-time.core :refer (now)]
    [clj-time.coerce :as c]
    [riemann.client :as r]
+   [clojure.set :refer (rename-keys)]
    [mount.core :as mount :refer (defstate)]))
 
 (defstate riemann
   :start (r/tcp-client (conf/get! :riemann))
   :stop (r/close! riemann))
 
-(defn send-event [m]
-  (r/send-event riemann (assoc m :time (c/to-epoch (now)))))
+(defn stat-events [{:keys [type stats timestamp] :as m}]
+  (map
+   (fn [[k v]]
+     (merge {:service k :time timestamp :metric v}
+            (select-keys m #{:tags :code :type :host}))) (stats type)))
+
+(defmethod into-events
+  (fn [{:keys [type]}] (keyword type)))
+
+(defmulti into-events :cpu [m]
+  (stat-events m))
+
+(defmulti into-events :default [m] m)
+
+(defn send-event [e]
+  (r/send-event riemann e))
