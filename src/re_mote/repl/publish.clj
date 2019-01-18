c9f0e5ad2b43dead9d84a6743545e2c7bcf68076
ronen <narkisr@gmail.com>
2019-01-19 00:13:12 +1100

    publishing events

--- src/re_mote/repl/publish.clj
+++ src/re_mote/repl/publish.clj
@@ -3,6 +3,7 @@
    ; publishing
    [postal.core :as p :refer (send-message)]
    [re-mote.publish.email :refer (template)]
+   [re-mote.publish.riemann :refer (send-event)]
    [re-share.config :as conf]
    [clojure.java.io :refer (file)]
    [clojure.core.strint :refer (<<)]
@@ -34,7 +35,13 @@
           message (merge e {:body (into [:alternative body] files)})]
       (send-message (conf/get! :re-mote :smtp) message)
       [this m]))
-  (riemann [this {:keys [success failure] :as m}]
+
+  (riemann [this {:keys [success failure] :as m} f]
+    (doseq [e success]
+      (send-event (assoc e :tags ["success"])))
+    (doseq [[code es] failure]
+      (doseq [e es]
+        (send-event (merge e {:tags ["failure"] :code code}))))
     [this m]))
 
 (defn refer-publish []
