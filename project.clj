4c55326748003c79146335ecdafe5fdf25043692
ronen <narkisr@gmail.com>
2019-02-03 00:56:08 +1100

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-mote "0.10.2"
+(defproject re-mote "0.10.3"
   :description "A live remote operations environment"
   :url "https://github.com/re-ops/re-mote"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
