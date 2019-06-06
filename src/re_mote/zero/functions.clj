c15b1e96d78d3c0e6962dde5dba5e7dd702b2ccf
ronen <narkisr@gmail.com>
2019-06-06 23:01:05 +1000

    moving fn-meta to cog

--- src/re_mote/zero/functions.clj
+++ src/re_mote/zero/functions.clj
@@ -1,19 +1,14 @@
 (ns re-mote.zero.functions
   (:require
    [taoensso.timbre :refer  (refer-timbre)]
-   [cheshire.core :refer (parse-string)]
    [re-mote.zero.send :refer (send-)]
    [re-mote.log :refer (gen-uuid)]
    [clojure.java.shell :refer [sh]]
+   [re-cog.core :refer (fn-meta)]
    [serializable.fn :as s]
    [me.raynes.fs :refer (list-dir tmpdir exists? file)]))
 
 (refer-timbre)
-
-; osquery
-(def ^{:doc "Run an osquery query"} osquery
-  (s/fn [query]
-    (parse-string (:out (sh "/usr/bin/osqueryi" "--json" query)) true)))
 
 ; Misc
 (def ^{:doc "list dir"} listdir
@@ -29,21 +24,15 @@
 
 (defn refer-zero-fns []
   (require '[re-mote.zero.functions :as fns :refer
-             (shell listdir call named)]))
-
-(defn fn-meta [f]
-  (meta
-   (second
-    (first
-     (filter #(and (var? (second %)) (= f (var-get (second %)))) (ns-map 're-mote.zero.functions))))))
+             (listdir call named)]))
 
 (defn call
   "Launch a remote clojure serializable functions on zeromq hosts"
   [f args zhs]
-  {:pre [(not (nil? zhs))]}
+  {:pre [(not (nil? zhs)) (-> f fn-meta :name)]}
   (let [uuid (gen-uuid)]
     (doseq [[hostname address] zhs]
-      (send- address {:request :execute :uuid  uuid :fn f :args args :name (-> f fn-meta :name)}))
+      (send- address {:request :execute :uuid  uuid :fn f :args args :name (-> f fn-meta :name keyword)}))
     uuid))
 
 (comment
