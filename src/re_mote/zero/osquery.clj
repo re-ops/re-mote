72ca69947a766831840bdba9b855f938689bf26a
ronen <narkisr@gmail.com>
2019-06-06 23:00:43 +1000

    using cog

--- src/re_mote/zero/osquery.clj
+++ src/re_mote/zero/osquery.clj
@@ -1,7 +1,7 @@
 (ns re-mote.zero.osquery
   (:require
    [re-mote.zero.pipeline :refer (run-hosts)]
-   [re-mote.zero.functions :refer (osquery)]
+   [re-cog.facts.query :refer (osquery)]
    re-mote.repl.base)
   (:import [re_mote.repl.base Hosts]))
 
