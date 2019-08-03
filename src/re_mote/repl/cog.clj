b4987def2b0dc467f2f32db7265b40b3e3c0c2e6
ronen <narkisr@gmail.com>
2019-08-04 00:16:47 +1000

    result combine

--- src/re_mote/repl/cog.clj
+++ src/re_mote/repl/cog.clj
@@ -1,7 +1,7 @@
 (ns re-mote.repl.cog
   "Running re-cog functions on hosts"
   (:require
-   [com.rpl.specter :refer (ALL MAP-VALS select multi-path filterer)]
+   [com.rpl.specter :refer (ALL MAP-VALS transform select multi-path filterer)]
    [re-mote.zero.pipeline :refer (run-hosts)]
    [re-cog.plan :refer (execution-plan)]
    re-mote.repl.base)
@@ -11,15 +11,27 @@
   (run-inlined [this _ f args])
   (run-plan [this _ p args]))
 
-(defn run-step [this args {:keys [succesful] :as m} f]
-  "We run the function on the last succesful hosts, failing hosts are filtered out from the result."
-  (let [f' (deref (resolve (symbol f)))
-        next-hosts (update this :hosts succesful)
-        {:keys [failure success] :as current} (run-hosts next-hosts f' args [5 :minute])
-        failing (into #{} (select [MAP-VALS :host] failure))]
+(defn combine-success [{:keys [success failure] :as acc} f curr-success]
+  (if success
+    (let [host-results (into {} (map (fn [{:keys [result host]}] {host {f result}}) curr-success))]
+      (mapv (fn [{:keys [host] :as v}] (update v :result (partial merge (host-results host)))) success))
+    (transform [ALL :result] (fn [v] {f v}) curr-success)))
+
+(defn combine-results [f {:keys [hosts] :as m} {:keys [failure success]}]
+  (let [failing (into #{} (select [MAP-VALS ALL :host] failure))]
     (->  m
-         (update :failure (partial merge-with conj))
-         (update :succesful (filter (comp not failing) succesful)))))
+         (combine-success f success)
+         (update :failure (fn [v] (merge-with conj (or v {}) failure)))
+         (assoc :hosts (filter (comp not failing) hosts)))))
+
+(defn run-recipe [args]
+  "We run a recipe function and merge its results into the accumulated results which include:
+    * The next hosts that are were successful in this round under :hosts
+    * Combined failures under :failure
+    * A combined success result per hosts with all of its results merged per recipe function
+  "
+  (fn [results f]
+    (combine-results f results (run-hosts (results :hosts) (deref (resolve (symbol f))) args [5 :minute]))))
 
 (extend-type Hosts
   ReCog
@@ -30,9 +42,7 @@
     (let [namespaces (deref (resolve (symbol p)))]
       (doseq [n namespaces]
         (require n))
-      (let [steps (execution-plan namespaces)
-            results (reduce (partial run-step this args) {:succesful (this :hosts)})]
-        [this]))))
+      [this (reduce (run-recipe args) {:succesful (this :hosts)} (execution-plan namespaces))])))
 
 (defn refer-cog []
   (require '[re-mote.repl.cog :as cog :refer (run-inlined run-plan)]))
