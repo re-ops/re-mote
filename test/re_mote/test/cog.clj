b644138f2f0aac9b1b27df94db093d6c19581562
ronen <narkisr@gmail.com>
2019-08-24 00:23:15 +1000

    adjusting out structure and adding spec test

--- test/re_mote/test/cog.clj
+++ test/re_mote/test/cog.clj
@@ -1,5 +1,6 @@
 (ns re-mote.test.cog
   (:require
+   [re-mote.spec :as rms :refer (valid?)]
    [re-mote.repl.cog :refer (combine-results)])
   (:use clojure.test))
 
@@ -7,30 +8,34 @@
                               :error {:out "host re-gent not connected"}
                               :host "a"
                               :uuid "4847d752d2b24cf28c57ffc06e569a7e"}]
-                         -2 [{:code -2
-                              :error {:out "No permissions"}
-                              :host "b"
-                              :uuid "4847d752d2b24cf2ffc06e569a7e5432"}]}
+                         2 [{:code 2
+                             :error {:out "No permissions"}
+                             :host "b"
+                             :uuid "4847d752d2b24cf2ffc06e569a7e5432"}]}
                :hosts ["a" "b" "c" "d"]
                :success [{:code 0
                           :host "c"
                           :profile {:time 0.091009881}
-                          :result {:profile #{{:end 975525241689
-                                               :result false
-                                               :start 975525185967
-                                               :time 5.5722E-5
-                                               :type :directory
-                                               :uuid "6b6cf853c6f24ad7b57122afe72fd3ed"}}}
+                          :result {:resources #{{:end 975525241689
+                                                 :err ""
+                                                 :out ""
+                                                 :exit 0
+                                                 :start 975525185967
+                                                 :time 5.5722E-5
+                                                 :type "directory"
+                                                 :uuid "6b6cf853c6f24ad7b57122afe72fd3ed"}}}
                           :uuid "4847d752d2b24cf28c57ffc06e569a7e"}
                          {:code 0
                           :host "d"
                           :profile {:time 1.5}
-                          :result {:profile #{{:end 975525241689
-                                               :result false
-                                               :start 975525185967
-                                               :time 5.5722E-5
-                                               :type :directory
-                                               :uuid "6b6cf853c6f24ad7b57122afe72fd3ed"}}}
+                          :result {:resources #{{:end 975525241689
+                                                 :err ""
+                                                 :out ""
+                                                 :exit 0
+                                                 :start 975525185967
+                                                 :time 5.5722E-5
+                                                 :type "directory"
+                                                 :uuid "6b6cf853c6f24ad7b57122afe72fd3ed"}}}
                           :uuid "4847d752d2b24cf28c57ffc06e569a7e"}]})
 
 (def output-2 {:failure {-1 [{:code -1
@@ -41,13 +46,24 @@
                :success [{:code 0
                           :host "d"
                           :profile {:time 3.2}
-                          :result {:profile #{{:end 975525241689
-                                               :result {:out "download ok"}
-                                               :start 975525185969
-                                               :time 10
-                                               :type :download
-                                               :uuid "7b6cf853c6f24ad7b57122afe72fd3ed"}}}
+                          :result {:resources #{{:end 975525241689
+                                                 :err ""
+                                                 :out "download ok"
+                                                 :exit 0
+                                                 :start 975525185969
+                                                 :time 10
+                                                 :type "download"
+                                                 :uuid "7b6cf853c6f24ad7b57122afe72fd3ed"}}}
                           :uuid "4847d752d2b24cf28c57ffc06e569a7e"}]})
+
+(deftest valid-results
+  (is (= (valid? :re-mote.spec/operation-result output-1) true))
+  (is (= (valid? :re-mote.spec/operation-result output-2) true)))
+
+(deftest non-valid-results
+  (with-out-str
+    (is (= (valid? :re-mote.spec/operation-result
+                   (assoc-in output-1 [:success 0 :result :resources] #{{:end 1 :start 2 :result 1}})) false))))
 
 (deftest single-host-sucessful
   (let [initial {:hosts ["a" "b" "c" "d"]}
